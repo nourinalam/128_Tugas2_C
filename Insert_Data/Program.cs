b0ecb3eb5ead935687b6d6ddc57f5419473c1229
nourinalam <nourinalam26@gmail.com>
2022-03-26 02:43:25 +0700

    insert data pada tabel kasir

--- Insert_Data/Program.cs
+++ Insert_Data/Program.cs
@@ -16,7 +16,34 @@ namespace Insert_Data
 
         public void InsertData()
         {
+            SqlConnection con = null;
+            try
+            {
+                con = new SqlConnection("data source =LAPTOP-8JHE7APE\\ALAM;database=Toko_Bakery_Alam;UserID-sa;password=123456");
+                con.Open();
 
+                SqlCommand cm = new SqlCommand("insert into Kasir(Id_Kasir, Nama_Kasir, Jenis_Kelamin) values('K01', 'Nourin', 'L')"
+                    + "insert into Kasir(Id_Kasir, Nama_Kasir, Jenis_Kelamin) values ('K02', 'Alam', 'L')"
+                    + "insert into Kasir(Id_Kasir, Nama_Kasir, Jenis_Kelamin) values ('K03', 'Adzani', 'L')"
+                    + "insert into Kasir(Id_Kasir, Nama_Kasir, Jenis_Kelamin) values ('K04', 'Putri', 'P')"
+                    + "insert into Kasir(Id_Kasir, Nama_Kasir, Jenis_Kelamin) values ('K05', 'Sindi', 'P')",
+
+                    
+                    con);
+                cm.ExecuteNonQuery();
+
+                Console.WriteLine("Sukses menambahkan data");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Gagal menambahkan data. " + e);
+                Console.ReadKey();
+            }
+            finally
+            {
+                con.Close();
+            }
         }
     }
 }
