35ec84c8e61fd3983ea934e501ad0d1f61a9b0ad
nourinalam <nourinalam26@gmail.com>
2022-03-26 01:32:00 +0700

    Add project files.

---
+++ Insert_Data/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Insert_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().InsertData();
+        }
+
+        public void InsertData()
+        {
+
+        }
+    }
+}
